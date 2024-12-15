5f7320bae049b0937608c0c2904d1297ef61bf52
fullStackAnsh <anshkanojia13@gmail.com>
2024-12-15 16:56:11 +0530

    map and filter

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-//trust issues with setTimout
-console.log("start");
-setTimeout(()=>{console.log("setTiemout")},0);
-console.log('end');
+let users =[
+    {firstname:"ansh",lastname:"kanojia",age:20},
+    {firstname:"joseph",lastname:"mishra",age:25},
+    {firstname:"john",lastname:"jhons",age:14},
+    {firstname:"selena",lastname:"sharma",age:60},
+]
+//users firstname with age less than 30 
+let output = users.filter((user)=>user.age<30).map((user)=>user.firstname)
+console.log(output);
