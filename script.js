9c7295918b59b1054c4dda2bfd075c1fa4f87d57
fullStackAnsh <anshkanojia13@gmail.com>
2024-12-15 14:46:14 +0530

    function statement and expression

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-let a = 100;
-var b = 200;
-{
-    let a =10;
-    var b =20;
-    console.log(a);
-    console.log(b);
+a(); // hoisted
+b(); //typeError
+// function statement 
+function a(){
+    console.log("a");
 }
-console.log(a);
-console.log(b);
+//function expression
+var b=function(){
+    console.log("b");
+}
