5790588f32fa542543ceba0b34b4e8fa7664176d
fullStackAnsh <anshkanojia13@gmail.com>
2024-12-15 16:21:06 +0530

    setTimeout at 0 sec

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-// event listeners and callback functions
-function event(){
-    document.getElementById('click').addEventListener('click',function xyz(){console.log("Clicked");});
-}
-event(); //xyz forms a closure with event , xyz is a callback function here
+//trust issues with setTimout
+console.log("start");
+setTimeout(()=>{console.log("setTiemout")},0);
+console.log('end');
